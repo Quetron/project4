d6a79afe15715070e0cc4ab1895581b0da34d87a
Quetron <060776@swcsd.us>
2022-05-09 12:39:56 -0400

    project4Finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,12 @@ Project 2
  */
 package com.company;
 
+import java.io.*;
 import java.util.*;
 
 import static java.lang.Integer.parseInt;
+
+import com.google.gson.Gson;
 
 public class Main {
     static boolean isValid;
@@ -17,8 +20,32 @@ public class Main {
     static List<Tasks> tasksArrayList = new ArrayList<>();
     static int parsedNumber;
 
+    static Gson gson = new Gson();
+    static File saveFile = new File("data.json");
+    static FileWriter fileWriter;
+
+    static {
+        try (FileReader reader = new FileReader(saveFile)) {
+            int content;
+            while ((content = reader.read()) != -1) {
+                System.out.print((char) content);
+            }
+            System.out.println();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static {
+        try {
+            fileWriter = new FileWriter("data.json");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     static void menuSelection() {
-        try{
+        try {
             System.out.println("      Menu      " +
                     "\n1) Add a task" +
                     "\n2) Remove a task" +
@@ -31,16 +58,16 @@ public class Main {
             System.out.print("Enter a number here: ");
             String usrNumber = input.nextLine();
             isValid = false;
-            while(!isValid){
-                try{
+            while (!isValid) {
+                try {
                     parsedNumber = parseInt(usrNumber);
-                    if(parsedNumber > 5 || parsedNumber < 0){
+                    if (parsedNumber > 5 || parsedNumber < 0) {
                         System.out.println("Enter a number 0-5");
                         usrNumber = input.nextLine();
-                    }else{
+                    } else {
                         isValid = true;
                     }
-                }catch(Exception e){
+                } catch (Exception e) {
                     System.out.println("Enter a number 0-5");
                     usrNumber = input.nextLine();
                 }
@@ -48,41 +75,41 @@ public class Main {
             switch (parsedNumber) {
                 case 1:
                     addTask();
-                    counter ++;
+                    counter++;
                     System.out.println();
                     menuSelection();
                     break;
                 case 2:
-                    if (tasksArrayList.size() == 0){
+                    if (tasksArrayList.size() == 0) {
                         System.out.println("Please add a task first");
-                    }else{
+                    } else {
                         removeTask();
                     }
                     System.out.println();
                     menuSelection();
                     break;
                 case 3:
-                    if (tasksArrayList.size() == 0){
+                    if (tasksArrayList.size() == 0) {
                         System.out.println("Please add a task first");
-                    }else{
+                    } else {
                         updateTask();
                     }
                     System.out.println();
                     menuSelection();
                     break;
                 case 4:
-                    if (tasksArrayList.size() == 0){
+                    if (tasksArrayList.size() == 0) {
                         System.out.println("Please add a task first");
-                    }else{
+                    } else {
                         printTasks();
                     }
                     System.out.println();
                     menuSelection();
                     break;
                 case 5:
-                    if (tasksArrayList.size() == 0){
+                    if (tasksArrayList.size() == 0) {
                         System.out.println("Please add a task first");
-                    }else{
+                    } else {
                         printByPriority();
                     }
                     System.out.println();
@@ -91,27 +118,36 @@ public class Main {
                 case 0:
                     //Closes the program
                     System.out.println("Thanks!");
+
+                    String json = gson.toJson(tasksArrayList);
+                    try {
+                        gson.toJson(tasksArrayList, fileWriter);
+                        fileWriter.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
                     break;
+
             }
         }
         //Catch anything errors that might randomly occur
-        catch (Exception e){
+        catch (Exception e) {
             System.out.println("Something went wrong, please try again.  Error: " + e);
             menuSelection();
         }
     }
 
-    static void addTask() {
+    static void addTask() throws IOException {
         System.out.println("What is the title of the task you want to add?: ");
         String titleOfTask = input.nextLine();
         isValid = false;
-        while(!isValid){
-            try{
+        while (!isValid) {
+            try {
                 Double.parseDouble(titleOfTask);
                 System.out.println("Please enter a string");
                 titleOfTask = input.nextLine();
-            }
-            catch(Exception e){
+            } catch (Exception e) {
                 isValid = true;
                 continue;
             }
@@ -119,13 +155,12 @@ public class Main {
         System.out.println("What is the description of the task you want to add?: ");
         String descriptionOfTask = input.nextLine();
         isValid = false;
-        while(!isValid){
-            try{
+        while (!isValid) {
+            try {
                 Double.parseDouble(descriptionOfTask);
                 System.out.println("Please enter a string");
                 descriptionOfTask = input.nextLine();
-            }
-            catch(Exception e){
+            } catch (Exception e) {
                 isValid = true;
                 continue;
             }
@@ -133,36 +168,36 @@ public class Main {
         System.out.println("What is the priority of the task?: ");
         String priorityOfTask = input.nextLine();
         isValid = false;
-        while(!isValid){
-            try{
+        while (!isValid) {
+            try {
                 int a = parseInt(priorityOfTask);
-                if(a > 5 || a < 0){
+                if (a > 5 || a < 0) {
                     System.out.println("Enter a number 0-5");
                     priorityOfTask = input.nextLine();
-                }else{
-                    newTask = new Tasks(titleOfTask, descriptionOfTask , a);
+                } else {
+                    newTask = new Tasks(titleOfTask, descriptionOfTask, a);
                     isValid = true;
                 }
-            }catch(Exception e){
+            } catch (Exception e) {
                 System.out.println("Not a valid input, please enter an Integer");
                 priorityOfTask = input.nextLine();
             }
         }
         tasksArrayList.add(newTask);
+
     }
 
 
-    static void removeTask(){
+    static void removeTask() {
         System.out.println("What is the index of the task you would like to remove?: ");
         String removeIndex = input.nextLine();
         boolean isRemoved = false;
-        while(!isRemoved){
-            try{
+        while (!isRemoved) {
+            try {
                 int parsedInt = parseInt(removeIndex);
                 tasksArrayList.remove(parsedInt);
                 isRemoved = true;
-            }
-            catch(Exception e){
+            } catch (Exception e) {
                 System.out.println("Not a valid input");
                 System.out.println("What is the index of the task you would like to remove?: ");
                 removeIndex = input.nextLine();
@@ -170,8 +205,8 @@ public class Main {
         }
     }
 
-    static void updateTask(){
-        try{
+    static void updateTask() {
+        try {
             System.out.println("what is the index of the task you would like to update?: ");
             String updateIndex = input.nextLine();
             int parseUpdateIndex = parseInt(updateIndex);
@@ -184,17 +219,17 @@ public class Main {
                     "\n3) Task Priority");
             String updatePicker = input.nextLine();
             isValid = false;
-            while (!isValid){
-                try{
+            while (!isValid) {
+                try {
                     parsedNumber = parseInt(updatePicker);
-                    if(parsedNumber > 3 || parsedNumber < 1){
+                    if (parsedNumber > 3 || parsedNumber < 1) {
                         System.out.println("not a valid input");
                         System.out.println("Please enter a number 1-3");
                         updatePicker = input.nextLine();
-                    }else{
+                    } else {
                         isValid = true;
                     }
-                }catch(Exception e){
+                } catch (Exception e) {
                     System.out.println("not a valid input");
                     System.out.println("What would you like to update?: " +
                             "\n1) Task Title" +
@@ -203,18 +238,17 @@ public class Main {
                     updatePicker = input.nextLine();
                 }
             }
-            switch (parsedNumber){
+            switch (parsedNumber) {
                 case 1:
                     System.out.println("What is the new title of the task?: ");
                     String updateTitle = input.nextLine();
                     isValid = false;
-                    while(!isValid){
-                        try{
+                    while (!isValid) {
+                        try {
                             Double.parseDouble(updateTitle);
                             System.out.println("Please enter a string");
                             updateTitle = input.nextLine();
-                        }
-                        catch(Exception e){
+                        } catch (Exception e) {
                             tasksArrayList.get(parseUpdateIndex).setTaskTitle(updateTitle);
                             isValid = true;
                             continue;
@@ -225,13 +259,12 @@ public class Main {
                     System.out.println("What is the new description of the task?: ");
                     String updateDescription = input.nextLine();
                     isValid = false;
-                    while(!isValid){
-                        try{
+                    while (!isValid) {
+                        try {
                             Double.parseDouble(updateDescription);
                             System.out.println("Please enter a string");
                             updateDescription = input.nextLine();
-                        }
-                        catch(Exception e){
+                        } catch (Exception e) {
                             tasksArrayList.get(parseUpdateIndex).setTaskDescription(updateDescription);
                             isValid = true;
                             continue;
@@ -242,29 +275,29 @@ public class Main {
                     System.out.println("What is the priority of the task?: ");
                     String updatePriority = input.nextLine();
                     isValid = false;
-                    while(!isValid){
-                        try{
+                    while (!isValid) {
+                        try {
                             int a = parseInt(updatePriority);
-                            if(a > 5 || a < 0){
+                            if (a > 5 || a < 0) {
                                 System.out.println("Enter a number 0-5");
                                 updatePriority = input.nextLine();
-                            }else{
+                            } else {
                                 tasksArrayList.get(parseUpdateIndex).setTaskPriority(a);
                                 isValid = true;
                             }
-                        }catch(Exception e){
+                        } catch (Exception e) {
                             System.out.println("Not a valid input, please enter an Integer");
                             updatePriority = input.nextLine();
                         }
                     }
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println("Not a valid input, please enter an index number");
             updateTask();
         }
     }
 
-    static void printTasks(){
+    static void printTasks() {
         Collections.sort(tasksArrayList);
         for (int i = 0; i < tasksArrayList.size(); i++) {
             System.out.println("Index: " + i
@@ -274,21 +307,21 @@ public class Main {
         }
     }
 
-    static void printByPriority(){
+    static void printByPriority() {
         System.out.println("What is the priority you want to look up?: ");
         String priorityNum = input.nextLine();
         isValid = false;
-        while (!isValid){
-            try{
+        while (!isValid) {
+            try {
                 parsedNumber = parseInt(priorityNum);
-                if(parsedNumber > 5 || parsedNumber < 0){
+                if (parsedNumber > 5 || parsedNumber < 0) {
                     System.out.println("not a valid input");
                     System.out.println("Please enter a priority 0-5");
                     priorityNum = input.nextLine();
-                }else{
+                } else {
                     isValid = true;
                 }
-            }catch(Exception e){
+            } catch (Exception e) {
                 System.out.println("not a valid input");
                 System.out.println("What is the priority you want to look up?: ");
                 priorityNum = input.nextLine();
@@ -296,7 +329,7 @@ public class Main {
         }
         for (int i = 0; i < tasksArrayList.size(); i++) {
             int priorityToMatch = tasksArrayList.get(i).getTaskPriority();
-            if (priorityToMatch == parsedNumber){
+            if (priorityToMatch == parsedNumber) {
                 System.out.println("Index: " + i
                         + " | Task title: " + tasksArrayList.get(i).getTaskTitle()
                         + " | Task Description: " + tasksArrayList.get(i).getTaskDescription()
---
+++ src/com/company/tempArray.java
@@ -0,0 +1,6 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class tempArray extends ArrayList {
+}
